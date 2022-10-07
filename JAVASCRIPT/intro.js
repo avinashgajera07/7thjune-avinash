7a219b05aa4ab259273f80d35fd44482fe7dfa14
avinashgajera07 <avinashgajera02@gmail.com>
2022-10-07 13:36:44 +0530

    javascript

---
+++ JAVASCRIPT/intro.js
@@ -0,0 +1,6 @@
+alert("hello")
+document.write(2+2);
+document.body.style.backgroundColor="black"
+console.log(document.body.style)
+document.body.style.color="white"
+console.log(document.body.style)
